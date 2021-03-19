1b96e25dacaadd3ec19de779cbee151ec5da613b
Ashish-pal <ak987982@gmail.com>
2021-03-19 13:06:18 +0530

    Revert "APIs & DB Update"

--- application/models/Common_modal.php
+++ application/models/Common_modal.php
@@ -35,19 +35,6 @@ class Common_modal extends CI_Model {
         return $Query;
     }
     
-    public function checkRefCode($username,$side)
-    {
-        $this->db->select('*');
-        $this->db->from('customers');
-        $this->db->where('username',$username);
-        if($side)
-        {
-            $this->db->where('side',$side);
-        }
-        $Query=$this->db->get();
-        return $Query;
-    }
-    
     public function getGeneralSettingDetailsById($generalId)
     {
         $this->db->select('*');
@@ -57,47 +44,6 @@ class Common_modal extends CI_Model {
             $this->db->where('id',$generalId);
         }
         $Query=$this->db->get()->result_array();
-        return $Query;
-    }
-    
-    public function checkCustomerLoginDetails($emailLogin,$passwordLogin)
-    {
-        $this->db->select('*');
-        $this->db->from('customers');
-        $this->db->where("id>0 and password='$passwordLogin' and (BINARY(email)='$emailLogin' or BINARY(username)='$emailLogin') and status=1");        
-        $Query=$this->db->get();
-        return $Query;
-    }
-    
-    public function getLastUserName()
-    {
-        $this->db->select('username');
-        $this->db->from('customers');
-        $this->db->order_by('id','desc');
-        $this->db->limit(1);
-        $Query=$this->db->get();        
-        return $Query;
-    }
-    
-    public function checkUserIfExist($userId,$email)
-    {
-        $this->db->select('*');
-        $this->db->from('customers');
-        $this->db->where('email',$email);
-        if($userId>0)
-        {
-            $this->db->where('id!=',$userId);
-        }
-        $Query=$this->db->get();
-        return $Query;
-    }
-    
-    public function getPlansDetails($whereArr)
-    {
-        $this->db->select('*');
-        $this->db->from('plans');
-        $this->db->where($whereArr);
-        $Query=$this->db->get();
         return $Query;
     }
 }
